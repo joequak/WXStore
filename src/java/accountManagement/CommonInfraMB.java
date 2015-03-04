998a2255defe91f113a8e37b2a6909239371d30c
JoeQuak <joe.zaxx89@gmail.com>
2015-03-04 11:34:30 +0800

    initiliase the project, yet to custom the templates

---
+++ src/java/accountManagement/CommonInfraMB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accountManagement;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author ¿.¿.¿
+ */
+@ManagedBean
+@SessionScoped
+public class CommonInfraMB {
+
+    /**
+     * Creates a new instance of CommonInfraMB
+     */
+    public CommonInfraMB() {
+    }
+    
+}
